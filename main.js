aa90abcd8ac77b9283aa446bf6ea3adf95a5a99a
kuni <ahmad.baikuni17@pwk.undip.ac.id>
2024-04-28 01:29:39 +0700

    add and remove layer functions

--- main.js
+++ main.js
@@ -34,6 +34,55 @@ let map = new maplibregl.Map({
             46.41032845779952], // starting position [lng, lat]
         zoom: 14 // starting zoom
     });
+function addLayer(layer,type,source,style,title){
+    // Add a symbol layer
+    map.addLayer({
+        'id': layer,
+        'type': type,
+        'source': source,
+        'layout': style
+    })
+    // {
+    //     'icon-image': 'custom-marker',
+    //     'text-font': [
+    //         'Open Sans Semibold',
+    //         'Arial Unicode MS Bold'
+    //     ],
+    //     'text-offset': [0, 1.25],
+    //     'text-anchor': 'top'
+    // }
+    // When a click event occurs on a feature in the places layer, open a popup at the
+    // location of the feature, with description HTML from its properties.
+    map.on('click', layer, (e) => {
+        const coordinates = e.features[0].geometry.coordinates.slice();
+        const description = e.features[0].properties[title];
+
+        // Ensure that if the map is zoomed out such that multiple
+        // copies of the feature are visible, the popup appears
+        // over the copy being pointed to.
+        while (Math.abs(e.lngLat.lng - coordinates[0]) > 180) {
+            coordinates[0] += e.lngLat.lng > coordinates[0] ? 360 : -360;
+        }
+        new maplibregl.Popup()
+            .setLngLat(coordinates)
+            .setHTML(description)
+            .addTo(map);
+    })
+     // Change the cursor to a pointer when the mouse is over the places layer.
+    map.on('mouseenter', layer, () => {
+        map.getCanvas().style.cursor = 'pointer';
+    });
+
+    // Change it back to a pointer when it leaves.
+    map.on('mouseleave', layer, () => {
+        map.getCanvas().style.cursor = '';
+    });
+    // console.log(map.getSource(source)._data)
+    map.fitBounds(turf.bbox(map.getSource(source)._data))
+}
+function removeLayer(layer){
+    map.removeLayer(layer)
+}
 map.on('load', async () => {
     // Add an image to use as a custom marker
     const image = await map.loadImage('https://maplibre.org/maplibre-gl-js/docs/assets/osgeo-logo.png');
@@ -43,23 +92,20 @@ map.on('load', async () => {
         'type': 'geojson',
         'data': points
     })
-    console.log(points)
     // layers
-    // Add a symbol layer
-    map.addLayer({
-        'id': 'conferences',
-        'type': 'symbol',
-        'source': 'points',
-        'layout': {
-            'icon-image': 'custom-marker',
-            // get the year from the source's "year" property
-            // 'text-field': ['get', 'year'],
-            'text-font': [
-                'Open Sans Semibold',
-                'Arial Unicode MS Bold'
-            ],
-            'text-offset': [0, 1.25],
-            'text-anchor': 'top'
+    $('#points-layer-check').on('change', ()=>{
+        if ($('#points-layer-check').prop('checked')) {
+            addLayer('conferences','symbol','points',{
+                'icon-image': 'custom-marker',
+                'text-font': [
+                    'Open Sans Semibold',
+                    'Arial Unicode MS Bold'
+                ],
+                'text-offset': [0, 1.25],
+                'text-anchor': 'top'
+            },'layer')
+        }else{
+            if (map.getLayer('conferences')) map.removeLayer('conferences')
         }
-    })
+   })  
 })
--- notes.md
+++ notes.md
@@ -1,12 +1,16 @@
 Requirements: 
--Maps should leverage OpenStreetMap 
--Support loading of data via geoJSON 
--Render Polygons, Points, and linestrings
+-Maps should leverage OpenStreetMap OK
+-Support loading of data via geoJSON  OK
+-Render Polygons, Points, and linestrings OK
 -Support colors, labels, and descriptions for each feature 
--Reactive design to display the map properly on both desktop and mobile devices 
--End user should be able to click on a point, polygon, or linestring and see a popup that has the description -End user should be able to zoom in/out/pan on the map -End user should be shown a navigation bar 
--Navigation bar should support grouping (in KML terms, folder) of features (points, polygons, linestrings) --Navigation bar should allow the ability to show/hide all features of a group or individual items 
--Navigation bar should allow the ability to "fly to" / center the map on the feature -ability to search the list of features from the geojson file 
+-Reactive design to display the map properly on both desktop and mobile devices OK
+-End user should be able to click on a point, polygon, or linestring and see a popup that has the description 
+-End user should be able to zoom in/out/pan on the map OK
+-End user should be shown a navigation bar 
+-Navigation bar should support grouping (in KML terms, folder) of features (points, polygons, linestrings) 
+-Navigation bar should allow the ability to show/hide all features of a group or individual items OK
+-Navigation bar should allow the ability to "fly to" / center the map on the feature OK
+-ability to search the list of features from the geojson file 
 -ability to show my current position (GPS) on the map 
 -ability to show directions from my current position to a selected point of interest
 -ability to show directions from a manually entered address to a selected point of interest -All source code to be provided (html, javascript, etc) upon project completion -Work should be branding free (excluding required copyrights such as openstreetmaps)  
